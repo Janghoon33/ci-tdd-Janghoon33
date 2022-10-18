b4d90f43fe180b961e464575516359601bee443a
Janghoon33 <h970303@naver.com>
2022-10-18 11:32:19 +0900

    TodoApp TodoList test ok

--- src/TodoApp_Janghoon33.js
+++ src/TodoApp_Janghoon33.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 const TodoApp_Janghoon33 = () => {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: 'TDD 배우기',
+            done: false,
+        },
+        {
+            id: 2,
+            text: 'react-testing-library',
+            done: true,
+        }
+    ]);
     return (<div>
         <TodoForm />
-        <TodoList todos={[]}/>
-
+        <TodoList todos={todos}/>
     </div>);
 };
 
--- src/TodoApp_Janghoon33.test.js
+++ src/TodoApp_Janghoon33.test.js
@@ -8,5 +8,10 @@ describe('<TodoApp_Janghoon33', () => {
             getByText('등록'); // TodoForm 화면에 있는지 확인
             getByTestId('TodoList'); // TodoList 화면에 있는지 확인
     });
+    it('renders two defaults todos',() => {
+        const {getByText} = render (<TodoApp_Janghoon33/>);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    })
 });
 
