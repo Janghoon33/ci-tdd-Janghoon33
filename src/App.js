028908e829339c9078f54a53e242124ca9013ed3
Janghoon33 <h970303@naver.com>
2022-10-18 12:41:47 +0900

    TodoApp ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './TodoForm';
+import TodoApp_Janghoon33 from './TodoApp_Janghoon33';
 
 function App() {
   return (
     <div className="App">
-      <TodoForm />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoApp_Janghoon33/>
+      Learn React
     </div>
   );
 }
