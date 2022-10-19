33dbca1999872bfe368af9ff5672447c9477c636
Janghoon33 <h970303@naver.com>
2022-10-19 14:43:52 +0900

    14:43

--- src/TodoApp_Janghoon33.js
+++ src/TodoApp_Janghoon33.js
@@ -39,7 +39,7 @@ const TodoApp_Janghoon33 = () => {
         );
     },[todos]);
     return (<div>
-        <h1>2022-10-19 14:17 Janghoon world!!!</h1>
+        <h1>2022-10-19 14:43 Janghoon world!!!</h1>
         <TodoForm onInsert={onInsert}/>
         <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
     </div>);
